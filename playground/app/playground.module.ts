ceaa099d126dcd219d261a2206dfbaecc9cb7f0c
Raymond Gigliotti <ray@firestitch.com>
2024-10-06 06:29:36 -0400

    removed legacy

--- playground/app/playground.module.ts
+++ playground/app/playground.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
 
 import { FsDiagramModule } from '@firestitch/diagram';
@@ -9,6 +8,8 @@ import { FsExampleModule } from '@firestitch/example';
 import { FsMenuModule } from '@firestitch/menu';
 import { FsMessageModule } from '@firestitch/message';
 import { FsZoomPanModule } from '@firestitch/zoom-pan';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { ExampleComponent, ExamplesComponent } from './components';
@@ -19,25 +20,25 @@ const routes: Routes = [
 ];
 
 @NgModule({
-    bootstrap: [AppComponent],
-    imports: [
-        BrowserModule,
-        FsDiagramModule,
-        BrowserAnimationsModule,
-        AppMaterialModule,
-        FormsModule,
-        FsZoomPanModule,
-        FsExampleModule.forRoot(),
-        FsMessageModule.forRoot(),
-        FsMenuModule.forRoot(),
-        RouterModule.forRoot(routes, {}),
-    ],
-    declarations: [
-        AppComponent,
-        ExamplesComponent,
-        ExampleComponent,
-    ],
-    providers: []
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    FsDiagramModule,
+    BrowserAnimationsModule,
+    AppMaterialModule,
+    FormsModule,
+    FsZoomPanModule,
+    FsExampleModule.forRoot(),
+    FsMessageModule.forRoot(),
+    FsMenuModule.forRoot(),
+    RouterModule.forRoot(routes, {}),
+  ],
+  declarations: [
+    AppComponent,
+    ExamplesComponent,
+    ExampleComponent,
+  ],
+  providers: [],
 })
 export class PlaygroundModule {
 }
